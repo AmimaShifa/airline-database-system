04e954e4e73d807f72e5d809ff5680fed7102625
Samson Lee <samson.lee@sjsu.edu>
2015-11-09 12:11:17 -0800

    - Added two other user requests -Added my SQL implementation for functional reqs 7-12

--- Src/Main.java
+++ Src/Main.java
@@ -10,7 +10,6 @@ public class Main
   public static void main(String[] argv) throws SQLException {
  
 	Connection connection = null;
-	Statement statement = null;
 	try {
 		//Change connection according to root info
 		connection = DriverManager
@@ -21,50 +20,97 @@ public class Main
 		return;
 	}
 	if (connection != null) {
-		System.out.println("You made it, take control your database now!");
+		System.out.println("Welcome to the SEK Airline Reservation System");
 	} else {
 		System.out.println("Failed to make connection!");
 	}	
-	/*
-	 * 
-	 * 
-	 * This is where we start to code!
-	 * 
-	 * 
-	 * 
-	 */
-	
-	//1st user request 
-	//very rough draft... 
-	//There will be a new tuple in booking with flight = 4
-	//run a query on mysql to verify
-	
 	//Map<seatNum, PassengerID>
 	Map<Integer, Integer> seatMap = new HashMap<Integer, Integer>();
 	Boolean loop = false;
 	while(!loop){
-			System.out.println("User Request: Create/Reserve flight for Passenger");
-			Scanner in = new Scanner(System.in);
-			System.out.print("passengerID?: ");
-			CallableStatement cs = connection.prepareCall("{CALL bookFlight(?, ?, ? , ?)}");
-			int pID = Integer.parseInt(in.nextLine());
-			cs.setInt(4, pID);
-			System.out.print("Class?: ");
-			cs.setString(3, in.nextLine());
-			System.out.print("FlightID?: ");
-			cs.setInt(1, Integer.parseInt(in.nextLine()));
-			System.out.print("SeatNum?: ");
-			int sID = Integer.parseInt(in.nextLine());
-			cs.setInt(2, sID);
-			seatMap.put(sID, pID);
-			ResultSet rs = cs.executeQuery();
-			System.out.println("Booking Created! Check DB to verify. Press X to exit, or N to reserve another flight\n");
-			if(in.nextLine().equals("X")){
-				loop = true;
-			}
+		Scanner in = new Scanner(System.in);
+		System.out.println("-Press 1 to Book/Reserve Flight for Passenger");
+		System.out.println("-Press 2 to Cancel Flight for Passenger");
+		System.out.println("-Press 3 to See Passengers by Flight");
+		System.out.println("-Press X to Exit");
+		String input = in.nextLine();
+		if(input.equals("1")){
+			loop = bookFlight(in, connection);
+		}else if (input.equals("2")){
+			loop = cancelFlight(in, connection);
+		}else if (input.equals("3")){
+			loop = getPassengersByFlight(in, connection);
+		}else if (input.equals("X")){
+			loop = true;
+		}
 	}		
 	
   }
   
+  //seatNum should be apart of primary Key so no two ticketIDs can have the same seatNum
+  //add this later
+  public static boolean bookFlight(Scanner in, Connection connection) throws SQLException{
+	  System.out.println("User Request: Create/Reserve flight for Passenger");
+		CallableStatement cs = connection.prepareCall("{CALL bookFlight(?, ?, ? , ?)}");
+		System.out.print("passengerID?: ");
+		int pID = Integer.parseInt(in.nextLine());
+		cs.setInt(4, pID);
+		System.out.print("Class?: ");
+		cs.setString(3, in.nextLine());
+		System.out.print("FlightID?: ");
+		cs.setInt(1, Integer.parseInt(in.nextLine()));
+		System.out.print("SeatNum?: ");
+		int sID = Integer.parseInt(in.nextLine());
+		cs.setInt(2, sID);
+		ResultSet rs = cs.executeQuery();
+		System.out.println("\nBooking Created! Check DB to verify.\n");
+	  return false;
+  }
+  
+  public static boolean cancelFlight(Scanner in, Connection connection) throws SQLException{
+	  System.out.println("User Request: Cancel flight for Passenger");
+		CallableStatement cs = connection.prepareCall("{CALL cancelFlight(?, ?, ? , ?)}");
+		System.out.print("passengerID?: ");
+		int pID = Integer.parseInt(in.nextLine());
+		cs.setInt(4, pID);
+		System.out.print("Class?: ");
+		cs.setString(3, in.nextLine());
+		System.out.print("FlightID?: ");
+		cs.setInt(1, Integer.parseInt(in.nextLine()));
+		System.out.print("SeatNum?: ");
+		int sID = Integer.parseInt(in.nextLine());
+		cs.setInt(2, sID);
+		ResultSet rs = cs.executeQuery();
+		System.out.println("\nBooking Cancelled! Check DB to verify.\n");
+	  return false;
+  }
+  
+  public static boolean getPassengersByFlight(Scanner in, Connection connection) throws SQLException{
+	  System.out.println("User Request - See Passengers By Flight");
+	  	CallableStatement cs = connection.prepareCall("{CALL getPassengersbyFlight(?)}");
+	  	System.out.print("FlightID?: ");
+	  	int fID = Integer.parseInt(in.nextLine());
+	  	cs.setInt(1, fID);
+	  	ResultSet rs = cs.executeQuery();
+	  	if (!rs.isBeforeFirst() ) {    
+	  		 System.out.println("\nThere are no passengers on Flight " + fID + " yet.\n");
+	  	}else{
+	  		System.out.println("\nHere is the list of passengers on Flight " + fID + ":");
+	  		printPassengersbyFlight(rs);
+	  	}	
+	  	
+	  return false;
+  }
+  
+  public static void printPassengersbyFlight(ResultSet rs) throws SQLException
+  {
+	  while(rs.next())
+	  {
+		  int fID = rs.getInt("flightID");
+		  String fName = rs.getString("firstName");
+		  String lName = rs.getString("lastName");
+		  System.out.println("Name: " + fName + " " + lName);
+	  }
+  }
   
 }
