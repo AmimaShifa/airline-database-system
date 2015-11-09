029060cbf415150c53839069bf3778db5af2f604
Samson Lee <samson.lee@sjsu.edu>
2015-11-09 12:25:39 -0800

    hm

--- FlightReservation.sql
+++ FlightReservation.sql
@@ -1,63 +1,61 @@
 
-DROP DATABASE IF EXISTS FLIGHT_RESERVATION;
-CREATE DATABASE FLIGHT_RESERVATION;
-USE FLIGHT_RESERVATION;
+DROP DATABASE IF EXISTS Flight_Reservation;
+CREATE DATABASE Flight_Reservation;
+USE Flight_Reservation;
 
-DROP TABLE IF EXISTs PASSENGER;
-CREATE TABLE PASSENGER
+DROP TABLE IF EXISTs Passenger;
+CREATE TABLE Passenger
 (
     passengerID     INT AUTO_INCREMENT,
     firstName       VARCHAR(45),
     lastName        VARCHAR(45),
     email           VARCHAR(45),
     age             INT,
+    updatedOn 		TIMESTAMP NOT NULL
     
+    ON UPDATE 		CURRENT_TIMESTAMP,
     PRIMARY KEY(passengerID)
 );
 
-DROP TABLE IF EXISTS AIRPLANE;
-CREATE TABLE AIRPLANE
+DROP TABLE IF EXISTS Airline;
+CREATE TABLE Airline
 (
-    airplaneID      INT AUTO_INCREMENT,
-    carrier         VARCHAR(45),
-    tailNum         VARCHAR(10),
+    airlineID      	INT AUTO_INCREMENT,
+    name         	VARCHAR(45),
+    code         	VARCHAR(10),
+    country         VARCHAR(45),
     
-    PRIMARY KEY(airplaneID)
+    PRIMARY KEY(airlineID)
 );
 
-DROP TABLE IF EXISTS FLIGHT;
-CREATE TABLE FLIGHT
+DROP TABLE IF EXISTS Airport;
+CREATE TABLE Airport
 (
-    flightID        INT AUTO_INCREMENT,
-    departureTime   TIME DEFAULT '00:00:00',
-    departureDate   DATE DEFAULT '0000-00-00',
-    arrivalTime     TIME DEFAULT '00:00:00',
-    arrivalDate     DATE DEFAULT '0000-00-00',
-    origin          VARCHAR(45),
-    destination     VARCHAR(45),
-    airplaneID      INT,
+    airportID      	INT AUTO_INCREMENT,
+    name         	VARCHAR(45),
+    city         	VARCHAR(45),
+    country         VARCHAR(45),
+    code         	VARCHAR(10),
     
-    PRIMARY KEY(flightID),
-    FOREIGN KEY(airplaneID) 	REFERENCES AIRPLANE(airplaneID)   
+    PRIMARY KEY(airportID)
 );
 
-DROP TABLE IF EXISTS BOOKING;
-CREATE TABLE BOOKING
+DROP TABLE IF EXISTS Route;
+CREATE TABLE Route
 (
-    ticketID        INT AUTO_INCREMENT,
-    seatNum         VARCHAR(5),
-    class           VARCHAR(45),
-    flightID        INT,
-    passengerID     INT,
+	routeID				INT AUTO_INCREMENT,
+	airlineID			INT,
+    departureAirportID	INT,
+    arrivalAirportID	INT,
     
-    PRIMARY KEY(ticketID),
-    FOREIGN KEY(flightID) 		REFERENCES FLIGHT(flightID),
-    FOREIGN KEY(passengerID) 	REFERENCES PASSENGER(passengerID)
+    PRIMARY KEY(routeID),
+    FOREIGN KEY(airlineID) 				REFERENCES Airline(airlineID),
+    FOREIGN KEY(departureAirportID) 	REFERENCES Airport(airportID),
+    FOREIGN KEY(arrivalAirportID) 		REFERENCES Airport(airportID)
 );
 
-
-DROP TABLE IF EXISTS PILOT;
-CREATE TABLE PILOT
+DROP TABLE IF EXISTS Pilot;
+CREATE TABLE Pilot
 (
     pilotID         INT AUTO_INCREMENT,
     firstName       VARCHAR(45),
@@ -68,35 +66,62 @@ CREATE TABLE PILOT
 
 );
 
-DROP TABLE IF EXISTS FLIGHT_PILOT;
-CREATE TABLE FLIGHT_PILOT
+DROP TABLE IF EXISTS Flight;
+CREATE TABLE Flight
+(
+    flightID        INT AUTO_INCREMENT,
+    departureTime   TIME DEFAULT '00:00:00',
+    departureDate   DATE DEFAULT '0000-00-00',
+    arrivalTime     TIME DEFAULT '00:00:00',
+    arrivalDate     DATE DEFAULT '0000-00-00',
+    routeID      	INT,
+    
+    PRIMARY KEY(flightID),
+    FOREIGN KEY(routeID) 	REFERENCES Route(routeID)   
+);
+
+DROP TABLE IF EXISTS Booking;
+CREATE TABLE Booking
+(
+    ticketID        INT AUTO_INCREMENT,
+    flightID        INT,
+    seatNum         INT,
+    class           VARCHAR(45),
+    passengerID     INT,
+    
+    PRIMARY KEY(ticketID),
+    FOREIGN KEY(flightID) REFERENCES Flight(flightID),
+    FOREIGN KEY(passengerID) REFERENCES Passenger(passengerID)
+);
+
+DROP TABLE IF EXISTS Flight_Pilot;
+CREATE TABLE Flight_Pilot
 (
     flightID        INT,
     pilotID         INT,
     
-    FOREIGN KEY(flightID) 		REFERENCES FLIGHT(flightID),
-    FOREIGN KEY(pilotID) 		REFERENCES PILOT(pilotID)
+    FOREIGN KEY(flightID) 		REFERENCES Flight(flightID),
+    FOREIGN KEY(pilotID) 		REFERENCES Pilot(pilotID)
 );
 
 DROP TRIGGER IF EXISTS DeletePassenger;
 delimiter //
 CREATE TRIGGER DeletePassenger
-AFTER DELETE ON PASSENGER 
+AFTER DELETE ON Passenger 
 FOR EACH ROW
 BEGIN
-	DELETE FROM BOOKING WHERE passengerID = OLD.passengerID;
+	DELETE FROM Booking WHERE passengerID = OLD.passengerID;
 END;
 //
 delimiter ;
 
-
 DROP TRIGGER IF EXISTS DeleteFlight;
 delimiter //
 CREATE TRIGGER DeleteFlight
-AFTER DELETE ON FLIGHT
+AFTER DELETE ON Flight
 FOR EACH ROW
 BEGIN
-	DELETE FROM BOOKING WHERE flightID = OLD.flightID;
+	DELETE FROM Booking WHERE flightID = OLD.flightID;
 END;
 //
 delimiter ;
@@ -104,10 +129,10 @@ delimiter ;
 DROP TRIGGER IF EXISTS DeletePilot;
 delimiter //
 CREATE TRIGGER DeletePilot
-AFTER DELETE ON PILOT
+AFTER DELETE ON Pilot
 FOR EACH ROW
 BEGIN
-	DELETE FROM FLIGHT_PILOT WHERE pilotID = OLD.pilotID;
+	DELETE FROM flight_pilot WHERE pilotID = OLD.pilotID;
 END; 
 //
 delimiter ;
